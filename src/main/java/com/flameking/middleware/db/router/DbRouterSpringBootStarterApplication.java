19d09f34118d4c538433d1bce655bf89b0a72a2a
wangwei <2711554770@qq.com>
2023-08-08 21:42:40 +0800

    new project

---
+++ src/main/java/com/flameking/middleware/db/router/DbRouterSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.flameking.middleware.db.router;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbRouterSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbRouterSpringBootStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flameking/middleware/db/router/DbRouterSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flameking.middleware.db.router;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbRouterSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
